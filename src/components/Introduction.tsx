94a1d5b0f83c6337bc0c1df75f16d791fa173024
Aditya Jamuar <adityaj@geekyants.com>
2021-02-19 12:32:54 +0530

    Global styles added

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable header/header */
+import React from 'react';
+import {Col, Row} from 'react-bootstrap';
+
+const Introduction = () => {
+  return (
+    <Row>
+      <Col className="d-flex justify-content-center">
+        <p>A JS library for building high-performance forms in React</p>
+      </Col>
+    </Row>
+  );
+};
+
+export default Introduction;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,10 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
 import Head from '@docusaurus/Head';
+import styles from './styles.module.css';
+import {Col, Row} from 'react-bootstrap';
+import Introduction from '../components/Introduction';
 
 const features = [
   {
@@ -73,8 +75,20 @@ function Home() {
         <meta property="og:description" content="My custom description" />
         <meta charSet="utf-8" />
         <title>Formst</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Heebo:wght@100&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <Introduction />
+      {/* <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -90,8 +104,11 @@ function Home() {
           </div>
         </div>
       </header>
-      <main>
-        {features && features.length > 0 && (
+      <main> */}
+      {/* <Row>
+          <Col>Test</Col>
+        </Row> */}
+      {/* {features && features.length > 0 && (
           <section className={styles.features}>
             <div className="container">
               <div className="row">
@@ -106,8 +123,8 @@ function Home() {
               </div>
             </div>
           </section>
-        )}
-      </main>
+        )} */}
+      {/* </main> */}
     </Layout>
   );
 }
