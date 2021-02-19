d2e58094980ba77695969de899c84ca37f6ac424
Meenu Makkar <meenumakkar@Meenus-MacBook-Pro.local>
2021-02-19 11:29:20 +0530

    fold 2

--- src/pages/folds/Fold2.tsx
+++ src/pages/folds/Fold2.tsx
@@ -21,38 +21,37 @@ const features = [
   {
     title: 'Middleware and Interdependency',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: <>Intercept and manipulate the input or u</>,
+    description: <>Create responsive forms for your app with ease</>,
   },
   {
     title: 'Out of the Box',
     imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>Get various validations for all fields right out of the box.</>
-    ),
+    description: <>Build flat, stepper or nested forms</>,
   },
   {
     title: 'Easy on the bundle',
     imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: <>1.7kB. That's all.</>,
+    description: <>Use middleware to modify form behaviour</>,
   },
 ];
 
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--3')}>
+    <div
+      className={clsx(
+        'col col-2',
+        'text-center',
+        'bg-light',
+        'mx-5',
+        styles.borderRadius20,
+      )}>
       {imgUrl && (
         <div>
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      <h3
-        style={{
-          fontSize: '20px',
-          letterSpacing: 0,
-        }}>
-        {title}
-      </h3>
+      <h4>{title}</h4>
       <p>{description}</p>
     </div>
   );
@@ -60,15 +59,11 @@ function Feature({imageUrl, title, description}) {
 
 export default function Fold2() {
   return (
-    <main>
+    <main className={clsx('my-5')}>
       <div
-        className={clsx(
-          'container',
-          styles.textAlignCenter,
-          styles.mainContainer,
-        )}>
-        <h1 className={clsx('hero__title')}>Explore NativeBase</h1>
-        <div className={clsx(styles.subtitle)}>
+        className={clsx('container', 'text-center', 'w-50', 'py-5', 'mx-auto')}>
+        <div className={clsx('fw-bold', styles.size60)}>Explore NativeBase</div>
+        <div className={clsx(styles.size22)}>
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
           dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.
         </div>
